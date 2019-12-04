ebd4906bc9c4ad88ca06fe0b804e0306112d56e4
1000ship <cjstjdgur1234@gmail.com>
2019-12-04 22:19:13 +0900

    Fixed some bugs

--- res/js/main.js
+++ res/js/main.js
@@ -1,17 +1,41 @@
-$('#carousel').carousel({
-    interval: false,
-    keyboard: false,
-  });
-
 function locationHashChanged() {
     if( location.hash.length == 0 )
+    {
         $('#carousel').carousel(0);
+        youtube_player.stopVideo();
+    }
     else
         switch( location.hash )
         {
             case "#video":
                 $('#carousel').carousel(1);
+                if(youtube_player)
+                   youtube_player.playVideo();
+                break;
         }
 }
+window.onhashchange = locationHashChanged;
 
-  window.onhashchange = locationHashChanged;
+
+$('#carousel').carousel({
+    interval: false,
+    keyboard: false,
+});
+
+// for youtube 
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var youtube_player;
+function onYouTubeIframeAPIReady() {
+    youtube_player = new YT.Player('video-player', {
+        videoId: '1NS1yxYX6IM',
+        events: {
+            'onReady': function(e){
+                e.target.playVideo();
+            },
+        },
+        playerVars: {'autoplay':true, 'controls': 0},
+    });
+}
