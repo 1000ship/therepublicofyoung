eb2150fd2528a8232b2817ee9c38d88b085ee352
1000ship <cjstjdgur1234@gmail.com>
2019-12-15 13:06:02 +0900

    Add Live-page

--- res/js/main.js
+++ res/js/main.js
@@ -1,4 +1,3 @@
-
 $('#carousel').carousel({
     interval: false,
     keyboard: false,
@@ -11,9 +10,31 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var youtube_player;
+var live_player;
 function onYouTubeIframeAPIReady() {
     youtube_player = new YT.Player('video-player', {
         videoId: '1NS1yxYX6IM',
         playerVars: {'autoplay':0, 'controls': 0},
     });
+
+    live_player = new YT.Player('live-player', {
+        videoId: 'fnh4V3u9lA4',
+        playerVars: {'autoplay':0, 'controls': 0},
+    });
 }
+
+// $(function() {
+//     $.ajax({
+//         url:'../../text.txt',
+//         type:'get',
+//         dataType:'text',
+//         success: function(data) {
+//             //서버로부터 정상적으로 응답이 왔을 때 실행
+//             alert(1)
+//         },
+//         error: function(err) {
+//             //서버로부터 응답이 정상적으로 처리되지 못햇을 때 실행
+//             alert(2)
+//         }
+//     });
+// });
